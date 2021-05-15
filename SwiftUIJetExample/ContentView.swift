40abb0e01be728a3ba3f7959573b56c9debdfae1
vellovaherpuu <vello@teeserted.com>
2021-05-15 12:27:57 +0300

    Test component used in example app.

--- SwiftUIJetExample/ContentView.swift
+++ SwiftUIJetExample/ContentView.swift
@@ -6,11 +6,11 @@
 //
 
 import SwiftUI
+import SwiftUIJet
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        JetTestComponent()
     }
 }
 
