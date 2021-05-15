de7880272d9c7454efca5fce55d9e87e1b854fc5
vellovaherpuu <vello@teeserted.com>
2021-05-15 11:43:52 +0300

    Initial

---
+++ SwiftUIJetExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIJetExample
+//
+//  Created by Vello Vaherpuu on 15.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
