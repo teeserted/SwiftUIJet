a5251648edc5b4c4a04c0bca4a2fb7ff57a4b67a
vellovaherpuu <vello@teeserted.com>
2021-05-16 12:06:54 +0300

    Added gitignore

---
+++ SwiftUIJet/Components/JetAccordion.swift
@@ -0,0 +1,20 @@
+//
+//  JetAccordion.swift
+//  SwiftUIJet
+//
+//  Created by Vello Vaherpuu on 15.05.2021.
+//
+
+import SwiftUI
+
+struct JetAccordion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JetAccordion_Previews: PreviewProvider {
+    static var previews: some View {
+        JetAccordion()
+    }
+}
